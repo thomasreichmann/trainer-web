b244fc4e9449c5b68218337750a8f8aebfb754c7
Thomas <thomasreichmann@hotmail.com>
2021-09-05 17:07:25 -0300

    styling of bottom nav

--- src/bottomNav/BottomNav.tsx
+++ src/bottomNav/BottomNav.tsx
@@ -1,32 +1,26 @@
-import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
 import './BottomNav.scss';
 import Link from './link/Link';
+import { MdHome, MdPerson, MdShowChart } from 'react-icons/md';
 
 export interface BottomNavProps {
 	activeRoute?: string;
 }
 
 const BottomNav = (props: BottomNavProps) => {
-	let items = [
-		{
-			name: 'test1',
-			href: 'testurl1',
-		},
-		{
-			name: 'test2',
-			href: 'testurl2',
-		},
-	];
-
 	return (
 		<nav className="navbar">
-			{/* <NavLink to="/">Home</NavLink>
-			<NavLink to="/list">List</NavLink>
-			<NavLink to="/profile">profile</NavLink> */}
-			<Link path="/">Home</Link>
-			<Link path="/list">List</Link>
-			<Link path="/profile">Home</Link>
+			<Link path="/profile">
+				<MdPerson />
+				<span>Profile</span>
+			</Link>
+			<Link path="/">
+				<MdHome />
+				<span>Home</span>
+			</Link>
+			<Link path="/list">
+				<MdShowChart />
+				<span>List</span>
+			</Link>
 		</nav>
 	);
 };
--- src/bottomNav/link/Link.tsx
+++ src/bottomNav/link/Link.tsx
@@ -1,15 +1,15 @@
-import { NavLink } from 'react-router-dom';
+import { match, NavLink } from 'react-router-dom';
 import './Link.scss';
 
 interface LinkProps {
 	path: string;
-	children: string;
+	children: JSX.Element | JSX.Element[];
 }
 
 const Link = (props: LinkProps) => {
 	return (
-		<NavLink to={props.path} activeClassName="active">
-			{props.children}
+		<NavLink to={props.path} className="link ripple" activeClassName="active" exact>
+			<span className="link-wrapper">{props.children}</span>
 		</NavLink>
 	);
 };
