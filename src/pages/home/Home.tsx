cac46e698149f7bdce79e7d2cdbcb580040e26ba
Thomas <thomasreichmann@hotmail.com>
2021-09-06 02:54:32 -0300

    added basic graph to home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,22 @@
+import { Line } from 'react-chartjs-2';
+
+const data = {
+	labels: ['1', '2', '3', '4', '5', '6'],
+	datasets: [
+		{
+			label: '# of Votes',
+			data: [12, 19, 3, 5, 2, 3],
+			fill: false,
+			backgroundColor: 'rgb(255, 99, 132)',
+			borderColor: 'rgba(255, 99, 132, 0.2)',
+		},
+	],
+};
+
 const Home = () => {
 	return (
 		<div>
-			<header>
-				<span>Home</span>
-			</header>
+			<Line data={data}></Line>
 		</div>
 	);
 };
